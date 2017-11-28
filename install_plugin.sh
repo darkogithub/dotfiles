7acb1aa7863c795ca2f451950f00f88ae6dc95e1
darko.pancev <darko.pancev@vangochip.com.tw>
2017-11-28 19:11:59 +0800

    modify for linux and add install plugin script

---
+++ install_plugin.sh
@@ -0,0 +1,15 @@
+git clone git://github.com/tpope/vim-sensible.git
+git clone git://github.com/tpope/vim-fugitive.git
+git clone git://github.com/tpope/vim-dispatch.git
+git clone git://github.com/tpope/vim-endwise.git
+git clone git://github.com/tpope/vim-surround.git
+git clone git://github.com/tpope/vim-commentary.git
+git clone git://github.com/airblade/vim-gitgutter.git
+git clone git://github.com/mattn/emmet-vim.git
+git clone git://github.com/vim-airline/vim-airline.git
+git clone git://github.com/vim-airline/vim-airline-themes.git
+git clone git://github.com/altercation/vim-colors-solarized.git
+git clone git://github.com/scrooloose/nerdtree.git
+git clone git://github.com/ctrlpvim/ctrlp.vim.git
+git clone git://github.com/christoomey/vim-tmux-navigator.git
+
